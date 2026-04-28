4f04fea03f29ebb5d261662cd7397acf6beb308d
Aleks Spencer <aleks.spencer@arm.com>
2026-04-28 10:43:56 +0100

    FEAT: Added the add_desks, add_user, del_desk, and del_user functionality

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ import yaml
 import os
 import datetime
 
+
 app = Flask(__name__)
+# Flask app initialises tables and inputs starting data from yaml files
 Lib.init_db()
 Lib.init_all_data()
 app.secret_key= "dev-secret"
@@ -20,6 +22,11 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    '''
+    Login page,
+    Gets user input through login.html page,
+    Navigates user to dashboard screen if credentials are correct
+    '''
     if request.method == "GET":
         return render_template("login.html")
 
@@ -42,13 +49,19 @@ def login():
 
 @app.route("/dashboard")
 def dashboard():
+    '''
+    Dashboard page where user selects functionality they wish to use
+    '''
     if "user_id" not in session:
         return redirect(url_for("login"))
     
     return render_template("dashboard.html")
 
-@app.route("/desks", methods=["GET"])
+@app.route("/show_desks", methods=["GET"])
 def show_desks():
+    '''
+    Presents all desks in a table
+    '''
     db = Lib.get_db_connection()
     rows = db.execute(
         "SELECT * FROM Desks"
@@ -60,7 +73,44 @@ def show_desks():
 
     return render_template("desks.html", desks=desks)
 
-@app.route("/bookings")
+@app.route("/add_desks", methods=["POST", "GET"])
+def add_desks():
+    if request.method == "GET":
+        return render_template("add_desks.html")
+
+    db = Lib.get_db_connection()
+    with db:
+        db.execute(
+            "INSERT INTO Desks (name, location, floor) VALUES ( ?, ?, ?)",
+            [request.form["name"],
+             request.form["location"],
+             request.form["floor"],
+             ]
+            )
+    db.close()
+
+    return redirect(url_for("dashboard"))
+    
+
+@app.route("/del_desks", methods=["POST", "GET"])
+def del_desks():
+    if request.method == "GET":
+        return render_template("del_desks.html")
+
+    db = Lib.get_db_connection()
+    with db:
+        db.execute(
+            "DELETE FROM Desks WHERE name = ? AND location = ? AND floor = ?",
+            [request.form["name"],
+             request.form["location"],
+             request.form["floor"],
+             ]
+            )
+    db.close()
+
+    return redirect(url_for("dashboard"))
+
+@app.route("/show_bookings", methods=["GET"])
 def show_bookings():
     if "user_id" not in session:
         return redirect(url_for("login"))
@@ -99,6 +149,46 @@ def make_booking():
     db.close()
 
     return redirect(url_for("show_bookings"))
+
+@app.route("/del_booking")
+
+@app.route("/add_user", methods=["POST", "GET"])
+def add_user():
+    if request.method == "GET":
+        return render_template("add_user.html")
+
+    values = [
+        request.form["username"],
+        request.form["password"],
+        request.form["f_name"],
+        request.form["s_name"],
+        request.form["email"]
+    ]
+
+    db = Lib.get_db_connection()
+    with db:
+        db.execute(
+            f"INSERT INTO Users (username, password, f_name, s_name, email) VALUES ( ?, ?, ?, ?, ?)",
+            values
+        )
+    db.close()
+
+    return redirect(url_for("dashboard"))
+
+@app.route("/del_user", methods=["POST", "GET"])
+def del_user():
+    if request.method == "GET":
+        return render_template("del_user.html")
+
+    db = Lib.get_db_connection()
+    with db:
+        db.execute(
+            "DELETE FROM Users WHERE username = ?",
+            [request.form["username"]]
+            )
+    db.close()
+
+    return redirect(url_for("dashboard"))
 
 @app.route("/logout")
 def logout():
